a5e89498dc13110dfa4d4f369ca9c4bb34fbd165
Taioh Kubota <kubotataioh@gmail.com>
2022-06-01 16:29:38 -0400

    created unit tests for company model"
    ;

--- models/company.test.js
+++ models/company.test.js
@@ -85,6 +85,57 @@ describe("findAll", function () {
       },
     ]);
   });
+
+  test("works: by min-employees", async function(){
+    let companies = await Company.findAll({minEmployees: 2});
+    expect(companies).toEqual([{
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+    },
+    {
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+      },
+   ]);
+  });
+
+  test("works: max-employees", async function(){
+    let companies = await Company.findAll({maxEmployees:2});
+    expect(companies).toEqual([{
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+    },
+    {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img"
+      },
+    ]);
+  })
+
+  test("works: name", async function(){
+    let companies = await Company.findAll({name:"C1"})
+    expect(companies).toEqual([{
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+    },
+  ]);
+});
+
 });
 
 /************************************** get */
