31c61be3b915268f3e092897d755d6b6c561784e
Taioh Kubota <kubotataioh@gmail.com>
2022-06-08 17:14:38 -0400

    add middleware for checking User or Admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -56,8 +56,21 @@ function ensureAdmin(req,res,next){
     }
 }
 
+function ensureCorrectUserOrAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!(user && (user.isAdmin || user.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
   ensureAdmin,
+  ensureCorrectUserOrAdmin,
 };
